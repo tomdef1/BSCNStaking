298cd7634270452a219c259d0c2dd4b7b4f9f679
Tom_DeFi <106840127+tomdef1@users.noreply.github.com>
2023-11-30 23:28:20 +0000

    Update BSCNStaking_F3_Token.sol

--- BSCNStaking_F3_Token.sol
+++ BSCNStaking_F3_Token.sol
@@ -2,13 +2,9 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BSCN_F3_Staking is ERC20, Ownable {
-    constructor(address initialOwner)
-        ERC20("BSCN_F3_Staking", "BSCNF3")
-        Ownable(initialOwner)
-    {
+contract BSCN_F3_Staking is ERC20 {
+    constructor() ERC20("BSCN_F3_Staking", "BSCNF3") {
         _mint(msg.sender, 350000 * 10 ** decimals());
     }
 }
