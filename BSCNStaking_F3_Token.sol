fa11d93560de1f6c0b821fd1adb797060f8a2ffd
Tom_DeFi <106840127+tomdef1@users.noreply.github.com>
2023-12-04 22:06:33 +0000

    Update BSCNStaking_F3_Token.sol

--- BSCNStaking_F3_Token.sol
+++ BSCNStaking_F3_Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BSCN_F3_Staking is ERC20 {
-    constructor() ERC20("BSCN_F3_Staking", "BSCNF3") {
+    constructor() ERC20("BSCNF3Staking", "BSCNF3") {
         _mint(msg.sender, 350000 * 10 ** decimals());
     }
 }
